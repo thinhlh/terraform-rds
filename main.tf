e3f39bf4a2ae7ddb028b41172bd011c982987ee9
Jamie Le <Jamie_Le@mckinsey.com>
2023-05-03 10:25:48 +0700

    ref: refactor instance connect

--- main.tf
+++ main.tf
@@ -86,7 +86,8 @@ resource "aws_db_instance" "instance_rds" {
   username            = var.rds_username
   password            = var.rds_password
   skip_final_snapshot = true
-  db_subnet_group_name = aws_db_subnet_group.default.name
   publicly_accessible = true
+  db_subnet_group_name = aws_db_subnet_group.default.name
   vpc_security_group_ids = [ "${aws_security_group.sg_rds.id}" ]
+  
 }
